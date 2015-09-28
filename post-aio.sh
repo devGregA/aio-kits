868e98c3b58461ace1431ac5127898f84044c1a4
weezer <weezer.su@gmail.com>
2015-09-28 17:28:47 -0500

    Create post-aio.sh

---
+++ post-aio.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "source /etc/network/interfaces.d/*.cfg"  >> /etc/network/interfaces
+ifup lxcbr0
+ifup br-mgmt
+ifup br-vxlan
+ifup br-storage
+ifup br-vlan
+
+lxc-system-manage containers-start
+cd /etc/haproxy/conf.d
+pwd
+ip_addr=$(ifconfig eth0 | grep "inet " | awk -F'[: ]+' '{ print $4 }')
+echo $ip_addd
+find . -type f -exec sed -i "s/104.239.240.212/$ip_addr/g" {} \;
+/etc/init.d/haproxy restart
+
+cd /etc/openstack_deploy
+pwd
+find . -type f -exec sed -i "s/104.239.240.212/$ip_addr/g" {} \;
